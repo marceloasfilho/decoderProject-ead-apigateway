511de8289e88dbae8855a508cbc51c3b902c56d7
Marcelo Filho <marceloandradesilvafilho@gmail.com>
2023-08-16 21:39:21 -0300

    - Commit inicial

---
+++ src/main/java/com/github/marceloasfilho/decoderprojectapigateway/DecoderProjectApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.marceloasfilho.decoderprojectapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoderProjectApigatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DecoderProjectApigatewayApplication.class, args);
+    }
+
+}
