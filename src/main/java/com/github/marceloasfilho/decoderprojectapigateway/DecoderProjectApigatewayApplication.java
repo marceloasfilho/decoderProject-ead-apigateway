edfc50c3e5bdfdc656d187855d3396077190a912
marcelo.filho <marceloandradesilvafilho@gmail.com>
2023-08-17 23:03:57 -0300

    - Inclusão do api gateway como client eureka - Alteração das rotas dos serviços de course e authuser de localhost:port para link do load balance - Adição de configuração de Eureka para o API GATEWAY enxergar como registro

--- src/main/java/com/github/marceloasfilho/decoderprojectapigateway/DecoderProjectApigatewayApplication.java
+++ src/main/java/com/github/marceloasfilho/decoderprojectapigateway/DecoderProjectApigatewayApplication.java
@@ -2,12 +2,12 @@ package com.github.marceloasfilho.decoderprojectapigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DecoderProjectApigatewayApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DecoderProjectApigatewayApplication.class, args);
     }
-
 }
